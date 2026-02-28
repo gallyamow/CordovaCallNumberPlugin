4286703cc4315ab2aee893023d1e0824b3111937
Ramil Gallyamov <gallyamow@gmail.com>
2026-02-28 20:25:56 +0300

    fix: ios@10 completionHandler usage

--- src/ios/CFCallNumber.m
+++ src/ios/CFCallNumber.m
@@ -7,33 +7,52 @@
     return [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tel://"]];
 }
 
-- (void) callNumber:(CDVInvokedUrlCommand*)command {
-    
+- (void)callNumber:(CDVInvokedUrlCommand*)command {
     [self.commandDelegate runInBackground:^{
-        
-        __block CDVPluginResult* pluginResult = nil;
         NSString* number = [command.arguments objectAtIndex:0];
         number = [number stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-        
-        if(![number hasPrefix:@"tel:"]){
-            number =  [NSString stringWithFormat:@"tel:%@", number];
+
+        if (![number hasPrefix:@"tel:"]) {
+            number = [NSString stringWithFormat:@"tel:%@", number];
         }
 
-        // run in mainthread as below 
         dispatch_async(dispatch_get_main_queue(), ^{
-            if(![CFCallNumber available]) {
-                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"NoFeatureCallSupported"];
+            // check avail
+            if (![CFCallNumber available]) {
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                                  messageAsString:@"NoFeatureCallSupported"];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                return;
             }
-            else if(![[UIApplication sharedApplication] openURL:[NSURL URLWithString:number]]) {
-                // missing phone number
-                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"CouldNotCallPhoneNumber"];
-            } else {
+
+            NSURL *url = [NSURL URLWithString:number];
+
+            // if new API
+            if (@available(iOS 10.0, *)) {
+                [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:^(BOOL success) {
+                    CDVPluginResult* pluginResult;
+                    if (success) {
+                        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                    } else {
+                        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                         messageAsString:@"CouldNotCallPhoneNumber"];
+                    }
+                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                }];
+                return;
+            }
+
+            // fallback for old iOS
+            BOOL success = [[UIApplication sharedApplication] openURL:url];
+            CDVPluginResult* pluginResult;
+            if (success) {
                 pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+            } else {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                 messageAsString:@"CouldNotCallPhoneNumber"];
             }
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         });
-        // return result
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        
     }];
 }
 
--- www/CallNumber.js
+++ www/CallNumber.js
@@ -1,4 +1,4 @@
-var CallNumber = function(){};
+const CallNumber = function(){};
 
 CallNumber.prototype.callNumber = function(success, failure, number, bypassAppChooser){
     cordova.exec(success, failure, "CallNumber", "callNumber", [number, bypassAppChooser]);
@@ -10,10 +10,9 @@ CallNumber.prototype.isCallSupported = function(success, failure){
 
 //Plug in to Cordova
 cordova.addConstructor(function() {
-
     if (!window.Cordova) {
         window.Cordova = cordova;
-    };
+    }
 
     if(!window.plugins) window.plugins = {};
     window.plugins.CallNumber = new CallNumber();
